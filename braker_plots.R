2fd9f5c46d020178bce0d0657289fe6bac4771d0
Camilla Eldridge <camilla.jl.eldridge@gmail.com>
2021-10-03 18:46:03 +0100

    Update braker_plots.R

--- braker_plots.R
+++ braker_plots.R
@@ -32,18 +32,18 @@ Log_geneLen<-ggplot() + geom_histogram(aes(log(genL$V1)), bins = 1000, fill = "b
 Gene_len<-ggplot() + geom_histogram(aes(genL$V1), bins = 1000, fill = "blue") + geom_density(alpha = 0.4) + theme_classic() + labs(x ="Gene length(bp)", y = "Count") #title="Distribution of gene lengths"
 detach(genL)
 
-#Plot transcript support from augustus.
+#Read in perc support from augustus.
 perc<-data.frame(read.table(perc_support, header = FALSE))
 attach(perc)
 
-#Plot all support.
-all_support<-ggplot() + geom_histogram(aes(perc$V3), fill = "blue", bins = 100) + geom_density(alpha = 0.4) + theme_classic() + labs(x ="% support", y = "Count")#title="Distribution of transcript support (all sources) ",
+#Plot all support/ note this includes all sources e.g exons (which will be zero if only using rna-seq input) 
+#all_support<-ggplot() + geom_histogram(aes(perc$V3), fill = "blue", bins = 100) + geom_density(alpha = 0.4) + theme_classic() + labs(x ="% support", y = "Count")#title="Distribution of transcript support (all sources) ",
 
 #Plot intron support.
 intron_support<-ggplot() + geom_histogram(aes(perc$V4), fill = "blue", bins = 100) + geom_density(alpha = 0.4) + theme_classic() + labs(x ="% support", y = "Count")#title="Distribution of transcript support - Introns ",
 
 # arrange plots
-res <- marrangeGrob(list(Gene_len, Exon_len, Log_geneLen, Log_exonLen, Intron_len, all_support, Log_intLen, intron_support), nrow = 2, ncol = 2)
+res <- marrangeGrob(list(Gene_len, Exon_len, Log_geneLen, Log_exonLen, Intron_len, Log_intLen, intron_support), nrow = 2, ncol = 2)
 
 # Export summary stat plots pdf file
 ggexport(res, filename = "Summary_stats.pdf")
@@ -57,14 +57,14 @@ chroms2<-data.frame(chroms_subset[!grepl("unloc",chroms_subset$V1), ])
 #Get unique list of chromosome ids
 chroms3<-unique(chroms2[1])
 
-# PLot perc support
-chroms_support_all<-ggplot(chroms2, aes(chroms2$V3)) + geom_histogram(fill = "blue", bins = 30) + geom_density(alpha = 0.4) + theme_classic() + labs(title="Transcript support (all sources) ", x ="% support", y = "Count") + facet_wrap(~ chroms2$V1)
+# PLot perc support, again , will include exon support that is likely to be zero (if only using rna-seq)
+#chroms_support_all<-ggplot(chroms2, aes(chroms2$V3)) + geom_histogram(fill = "blue", bins = 30) + geom_density(alpha = 0.4) + theme_classic() + labs(title="Transcript support (all sources) ", x ="% support", y = "Count") + facet_wrap(~ chroms2$V1)
 
 # Plot intron support 
 chroms_support_intron<-ggplot(chroms2, aes(chroms2$V4)) + geom_histogram(fill = "blue", bins = 30) + geom_density(alpha = 0.4) + theme_classic() + labs(title="Transcript support (introns) ", x ="% support", y = "Count") + facet_wrap(~ chroms2$V1)
 
 # arrange plots
-res2 <- marrangeGrob(list(chroms_support_all, chroms_support_intron), nrow = 1, ncol = 1)
+res2 <- marrangeGrob(list(chroms_support_intron), nrow = 1, ncol = 1)
 
 # Export chromosome stats to pdf file
 ggexport(res2, filename = "Summary_stats_chromosomes.pdf")
